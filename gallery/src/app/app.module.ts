30e408faad37e8dd4486dde18532c1ae50270515
mgradalska <martagradalska@gmail.com>
2019-11-06 17:39:19 +0100

    Generated base app

---
+++ gallery/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NavigationComponent} from './navigation/navigation.component';
+import {MainPageComponent} from './main-page/main-page.component';
+import {FooterComponent} from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavigationComponent,
+    MainPageComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
